8f448a9ff0510901d463eaae093a3867b019725a
Devananda van der Veen <devananda.vdv@gmail.com>
2012-10-23 12:30:44 -0700

    lots of clean up in prepare-host script

--- scripts/prepare-devstack-for-baremetal.sh
+++ scripts/prepare-devstack-for-baremetal.sh
@@ -4,8 +4,8 @@
 set -e
 
 DEVSTACK_PATH=${DEVSTACK_PATH:-/home/stack/devstack}
-[ -e $DEVSTACK_PATH/openrc ] && \
-   source $DEVSTACK_PATH/openrc
+source $DEVSTACK_PATH/openrc
+source $DEVSTACK_PATH/localrc
 
 MYSQL_USER=${MYSQL_USER:-root}
 MYSQL_HOST=${MYSQL_HOST:-localhost}
@@ -29,18 +29,15 @@ BM_RAMDISK=${BM_RAMDISK:-bm-deploy-ramdisk.$KERNEL_VER.img}
 BM_RUN_KERNEL=${BM_RUN_KERNEL:-$BM_KERNEL}
 BM_RUN_RAMDISK=${BM_RUN_RAMDISK:-$BM_RAMDISK}
 
-# TODO: get these from localrc
+DNSMASQ_IFACE=${DNSMASQ_IFACE:-$PUBLIC_INTERFACE}
 DNSMASQ_IFACE=${DNSMASQ_IFACE:-eth0}
+
+# can we get this from localrc? it looks like FIXED_RANGE, but format is different
 DNSMASQ_RANGE=${DNSMASQ_RANGE:-192.168.2.33,192.168.2.63}
 
 BM_SCRIPT_PATH=${BM_SCRIPT_PATH:-/opt/stack/nova/bin}
 BM_SCRIPT=${BM_SCRIPT:-nova-baremetal-manage}
 BM_HELPER=${BM_HELPER:-nova-baremetal-deploy-helper}
-
-if [ ! -e $BM_SCRIPT_PATH/$BM_SCRIPT ]; then
-   echo "Failed to find bare metal management script"
-   exit 1
-fi
 
 BM_SERVICE_HOST_NAME=${BM_SERVICE_HOST_NAME:-`hostname`}
 BM_TARGET_MAC=${BM_TARGET_MAC:-01:23:45:67:89:01}
@@ -48,6 +45,14 @@ BM_TARGET_MAC=${BM_TARGET_MAC:-01:23:45:67:89:01}
 PM_ADDR=${PM_ADDR:-1.2.3.4}
 PM_USER=${PM_USER:-root}
 PM_PASS=${PM_PASS:-secret}
+
+
+function load_image {
+   retval=$($GLANCE --verbose add is_public=true container_format=$1 disk_format=$1 name=$2 < $IMG_PATH/$3)
+   id=$(echo "$retval" | head -1 | awk '{print $6}')
+   echo $id
+}
+
 set -o xtrace
 
 # build deployment ramdisk if needed 
@@ -72,33 +77,22 @@ $MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE IF NOT EXISTS nova_bm
 $MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -e "$sql"
 $MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -v -v -f nova_bm < $(dirname $0)/init_nova_bm_db.sql
 
-# load main AMI
-[ -e $IMG_PATH/$BM_IMAGE ] && \
-   ami=$($GLANCE --verbose add is_public=true container_format=ami disk_format=ami name=$BM_NODE_NAME < $IMG_PATH/$BM_IMAGE) && \
-   ami=$(echo "$ami" | head -1 | awk '{print $6}')
+ami=$(load_image "ami" $BM_NODE_NAME $BM_IMAGE)
+aki=$(load_image "aki" "aki-01" $BM_KERNEL)
+ari=$(load_image "ari" "ari-01" $BM_RAMDISK)
 
-# load deploy kernel & ramdisk
-[ -e $IMG_PATH/$BM_KERNEL ] && \
-   aki=$($GLANCE --verbose add is_public=true container_format=aki disk_format=aki name='aki-01' < $IMG_PATH/$BM_KERNEL) && \
-   aki=$(echo "$aki" | head -1 | awk '{print $6}')
-
-[ -e $IMG_PATH/$BM_RAMDISK ] && \
-   ari=$($GLANCE --verbose add is_public=true container_format=ari disk_format=ari name='ari-01' < $IMG_PATH/$BM_RAMDISK) && \
-   ari=$(echo "$ari" | head -1 | awk '{print $6}')
-
-#XXXXXXXXXXXx------------------ things work up to here -----------------
 # associate deploy aki and ari to main AMI
 $GLANCE image-update --property "deploy_kernel_id=$aki" $ami
 $GLANCE image-update --property "deploy_ramdisk_id=$ari" $ami
 
 # load run-time aki and ari, if specified
-[ -e $IMG_PATH/$BM_RUN_KERNEL ] && [ $BM_RUN_KERNEL != $BM_KERNEL ] && \
-   aki=$($GLANCE --verbose add is_public=true container_format=aki disk_format=aki name='aki-02' < $IMG_PATH/$BM_RUN_KERNEL) && \
-   aki=$(echo "$aki" | head -1 | awk '{print $6}')
+if [ $BM_RUN_KERNEL != $BM_KERNEL ]; then
+   aki=$(load_image "aki" "aki-02" $BM_RUN_KERNEL)
+fi
 
-[ -e $IMG_PATH/$BM_RUN_RAMDISK ] && [ $BM_RUN_RAMDISK != $BM_RAMDISK ] && \
-   ari=$($GLANCE --verbose add is_public=true container_format=ari disk_format=ari name='ari-02' < $IMG_PATH/$BM_RUN_RAMDISK) && \
-   ari=$(echo "$ari" | head -1 | awk '{print $6}')
+if [ $BM_RUN_RAMDISK != $BM_RAMDISK ]; then
+   ari=$(load_image "ari" "ari-02" $BM_RUN_RAMDISK)
+fi
 
 # associate run-time aki and ari to main AMI
 $GLANCE image-update --property "kernel_id=$aki" $ami
@@ -109,7 +103,7 @@ $NOVA_MANAGE instance_type create --name=$BM_NODE_NAME --cpu=1 --memory=512 --ro
 $NOVA_MANAGE instance_type set_key --name=$BM_NODE_NAME --key cpu_arch --value "x86_64"
 
 # restart dnsmasq
-sudo pkill dnsmasq
+sudo pkill dnsmasq || true
 sudo dnsmasq --conf-file= --port=0 --enable-tftp --tftp-root=/tftpboot --dhcp-boot=pxelinux.0 --bind-interfaces --pid-file=/var/run/dnsmasq.pid --interface=$DNSMASQ_IFACE --dhcp-range=$DNSMASQ_RANGE
 
 # sync nova_bm database
@@ -120,6 +114,7 @@ $BM_SCRIPT_PATH/$BM_SCRIPT db sync
 
 # make bare-metal DB aware of our HW node and any additional network interfaces
 $BM_SCRIPT_PATH/$BM_SCRIPT node create --host=$BM_SERVICE_HOST_NAME --cpus=1 --memory_mb=512 --local_gb=0 --pm_address=$PM_ADDR --pm_user=$PM_USER --pm_password=$PM_PASS --prov_mac=$BM_TARGET_MAC --terminal_port=0
+$BM_SCRIPT_PATH/$BM_SCRIPT interface create --node_id=1 --mac_address=$BM_FAKE_MAC --datapath_id=0 --port_no=0
 
 # add keypair... optional step
 $NOVA keypair-add --pub_key ~/.ssh/authorized_keys  default
