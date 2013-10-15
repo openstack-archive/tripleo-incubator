44169b7a6f9463e622ef6ee73cc14e7b055d18ad
Derek Higgins <derekh@redhat.com>
2013-10-15 23:44:07 +0100

    Call cleanup-env early in devtest
    Calling this ensures  VM's and storage from previous devtest runs are
    destroyed and undefined.
    
    Change-Id: I5c11eb07803228d21ccbfd5c4d34128399824b8f

--- scripts/devtest.sh
+++ scripts/devtest.sh
@@ -192,6 +192,12 @@ export NODE_DIST=${NODE_DIST:-ubuntu} #nodocs
 
 export DHCP_DRIVER=bm-dnsmasq
 
+## #. Run cleanup-env to ensure VM's and storage pools from previous devtest
+##    runs are removed.
+##    ::
+echo "Cleaning up vm's/storage from previous devtest runs" #nodocs
+cleanup-env
+
 ## #. Ensure dependencies are installed and required virsh configuration is
 ##    performed:
 ##    ::
