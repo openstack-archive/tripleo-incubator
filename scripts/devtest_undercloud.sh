625aa75d923f6a45becb8a242abf050ccc5a92a2
Mark McLoughlin <markmc@redhat.com>
2013-11-28 11:45:09 +0000

    Fix some rst formatting in devtest_undercloud
    The 'make -C' block isn't being properly block quoted.
    
    Change-Id: Ia8786142968c2dc734e125a9b06c0fec2396023b

--- scripts/devtest_undercloud.sh
+++ scripts/devtest_undercloud.sh
@@ -43,7 +43,8 @@ load-image $TRIPLEO_ROOT/undercloud.qcow2
 setup-undercloud-passwords
 source tripleo-undercloud-passwords
 
-## #. Deploy an undercloud::
+## #. Deploy an undercloud
+##    ::
 make -C $TRIPLEO_ROOT/tripleo-heat-templates undercloud-vm.yaml
 heat stack-create -f $TRIPLEO_ROOT/tripleo-heat-templates/undercloud-vm.yaml \
     -P "PowerUserName=$(whoami);AdminToken=${UNDERCLOUD_ADMIN_TOKEN};AdminPassword=${UNDERCLOUD_ADMIN_PASSWORD};GlancePassword=${UNDERCLOUD_GLANCE_PASSWORD};HeatPassword=${UNDERCLOUD_HEAT_PASSWORD};NeutronPassword=${UNDERCLOUD_NEUTRON_PASSWORD};NovaPassword=${UNDERCLOUD_NOVA_PASSWORD};BaremetalArch=${NODE_ARCH}" \
