671bc657a87fce159f8e45fef66ef107c88e1a2d
Clint Byrum <clint@fewbar.com>
2013-12-03 20:52:36 -0800

    Extend devtest_overcloud timeout by 5 minutes
    We have seen that the overcloud is taking a bit longer to come up for
    some reason. This change gives it 5 more minutes.
    
    Change-Id: I32dccf4649bec6b5301584d30d08b9fa961dc572

--- scripts/devtest_overcloud.sh
+++ scripts/devtest_overcloud.sh
@@ -125,7 +125,7 @@ fi #nodocs
 ##    ::
 
 echo "Waiting for the overcloud stack to be ready" #nodocs
-wait_for 190 10 stack-ready overcloud
+wait_for 220 10 stack-ready overcloud
 export OVERCLOUD_IP=$(nova list | grep notcompute.*ctlplane | sed  -e "s/.*=\\([0-9.]*\\).*/\1/")
 ### --end
 # If we're forcing a specific public interface, we'll want to advertise that as
