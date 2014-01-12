5675311e2db7b01b63ad2951f142497d1c240d7a
Steve Kowalik <steven@wedontsleep.org>
2014-01-12 15:08:33 +0800

    Populate devtest_variables from devtest_setup.
    Shift great hunks from devtest_setup (all of the things like exporting
    variables) into devtest_variables. This allows devleopers to just
    source devtest_variables into their environment to run the other
    scripts that depend on the variables existing in the environment.
    
    Change-Id: Idd412616a547c5af1c13ead8bcdc911a26c5527b

--- scripts/devtest_setup.sh
+++ scripts/devtest_setup.sh
@@ -126,32 +126,6 @@ fi
 ##    host machine and issuing libvirt/virsh commands. The user these instructions
 ##    use is your own, but you can also setup a dedicated user if you choose.
 
-## #. The devtest scripts require access to the libvirt system URI.
-##    If running against a different libvirt URI you may encounter errors.
-##    Export LIBVIRT_DEFAULT_URI to prevent devtest using qemu:///system
-##    Check that the default libvirt connection for your user is qemu:///system.
-##    If it is not, set an environment variable to configure the connection.
-##    This configuration is necessary for consistency, as later steps assume
-##    qemu:///system is being used.
-##    ::
-
-export LIBVIRT_DEFAULT_URI=${LIBVIRT_DEFAULT_URI:-"qemu:///system"}
-
-## #. The vm's created by devtest will use e1000 network device emulation by
-##    default.  This can be overriden to use a different network driver for
-##    interfaces instead, such as virtio.  virtio provides faster network
-##    performance than e1000, but may prove to be less stable.
-##    ::
-
-export LIBVIRT_NIC_DRIVER=${LIBVIRT_NIC_DRIVER:-"e1000"}
-
-## #. Choose a base location to put all of the source code.
-##    ::
-##         # exports are ephemeral - new shell sessions, or reboots, and you need
-##         # to redo them, or use $TRIPLEO_ROOT/tripleo-incubator/scripts/write-tripleorc
-##         # and then source the generated tripleorc file.
-##         export TRIPLEO_ROOT=~/tripleo
-export TRIPLEO_ROOT=${TRIPLEO_ROOT:-~/.cache/tripleo} #nodocs
 mkdir -p $TRIPLEO_ROOT
 cd $TRIPLEO_ROOT
 
@@ -176,62 +150,10 @@ fi
 
 export PATH=$TRIPLEO_ROOT/tripleo-incubator/scripts:$PATH
 
-## #. Set HW resources for VMs used as 'baremetal' nodes. NODE_CPU is cpu count,
-##    NODE_MEM is memory (MB), NODE_DISK is disk size (GB), NODE_ARCH is
-##    architecture (i386, amd64). NODE_ARCH is used also for the seed VM.
-##    A note on memory sizing: TripleO images in raw form are currently
-##    ~2.7Gb, which means that a tight node will end up with a thrashing page
-##    cache during glance -> local + local -> raw operations. This significantly
-##    impairs performance. Of the four minimum VMs for TripleO simulation, two
-##    are nova baremetal nodes (seed an undercloud) and these need to be 2G or
-##    larger. The hypervisor host in the overcloud also needs to be a decent size
-##    or it cannot host more than one VM.
-## 
-##    32bit VMs::
-## 
-##         export NODE_CPU=1 NODE_MEM=2048 NODE_DISK=20 NODE_ARCH=i386
-export NODE_CPU=${NODE_CPU:-1} NODE_MEM=${NODE_MEM:-2048} NODE_DISK=${NODE_DISK:-20} NODE_ARCH=${NODE_ARCH:-i386} #nodocs
-
-##    For 64bit it is better to create VMs with more memory and storage because of
-##    increased memory footprint::
-## 
-##         export NODE_CPU=1 NODE_MEM=2048 NODE_DISK=20 NODE_ARCH=amd64
-## 
-## #. Set distribution used for VMs (fedora, ubuntu).
-##    ::
-## 
-##         export NODE_DIST=ubuntu
-## 
-##    for Fedora set SELinux permissive mode.
-##    ::
-## 
-##         export NODE_DIST="fedora selinux-permissive"
-### --end
-
-source $TRIPLEO_ROOT/tripleo-incubator/scripts/set-os-type
-if [ "$TRIPLEO_OS_DISTRO" == 'unsupported' ]; then
+if [ "$NODE_DIST" == 'unsupported' ]; then
     echo 'Unsupported OS distro.'
     exit 1
 fi
-export NODE_DIST=${NODE_DIST:-"$TRIPLEO_OS_DISTRO"}
-
-### --include
-
-## #. Set the default bare metal power manager. By default devtest uses
-##    nova.virt.baremetal.virtual_power_driver.VirtualPowerManager to
-##    support a fully virtualized TripleO test environment. You may
-##    optionally customize this setting if you are using real baremetal
-##    hardware with the devtest scripts. This setting controls the
-##    power manager used in both the seed VM and undercloud.
-##    ::
-export POWER_MANAGER=${POWER_MANAGER:-'nova.virt.baremetal.virtual_power_driver.VirtualPowerManager'}
-
-## #. Set a list of image elements that should be included in all image builds.
-##    Note that stackuser is only for debugging support - it is not suitable for
-##    a production network. This is also the place to include elements such as
-##    pip-cache or pypi-openstack if you intend to use them.
-##    ::
-export DIB_COMMON_ELEMENTS=${DIB_COMMON_ELEMENTS:-"stackuser"}
 
 ## #. Ensure dependencies are installed and required virsh configuration is
 ##    performed:
@@ -256,10 +178,6 @@ fi
 if [ "$USE_CACHE" == "0" ] ; then #nodocs
     pull-tools
 fi #nodocs
-
-## #. You need to make the tripleo image elements accessible to diskimage-builder:
-##    ::
-export ELEMENTS_PATH=$TRIPLEO_ROOT/tripleo-image-elements/elements
 
 ## #. Configure a network for your test environment.
 ##    This configures an openvswitch bridge and teaches libvirt about it.
--- scripts/devtest_variables.sh
+++ scripts/devtest_variables.sh
@@ -3,3 +3,87 @@
 # Variable definition for devtest.
 
 export TE_DATAFILE=${TE_DATAFILE:-testenv.json}
+
+### --include
+## #. The devtest scripts require access to the libvirt system URI.
+##    If running against a different libvirt URI you may encounter errors.
+##    Export LIBVIRT_DEFAULT_URI to prevent devtest using qemu:///system
+##    Check that the default libvirt connection for your user is qemu:///system.
+##    If it is not, set an environment variable to configure the connection.
+##    This configuration is necessary for consistency, as later steps assume
+##    qemu:///system is being used.
+##    ::
+
+export LIBVIRT_DEFAULT_URI=${LIBVIRT_DEFAULT_URI:-"qemu:///system"}
+
+## #. The vm's created by devtest will use e1000 network device emulation by
+##    default.  This can be overriden to use a different network driver for
+##    interfaces instead, such as virtio.  virtio provides faster network
+##    performance than e1000, but may prove to be less stable.
+##    ::
+
+export LIBVIRT_NIC_DRIVER=${LIBVIRT_NIC_DRIVER:-"e1000"}
+
+## #. Choose a base location to put all of the source code.
+##    ::
+##         # exports are ephemeral - new shell sessions, or reboots, and you need
+##         # to redo them, or use $TRIPLEO_ROOT/tripleo-incubator/scripts/write-tripleorc
+##         # and then source the generated tripleorc file.
+##         export TRIPLEO_ROOT=~/tripleo
+export TRIPLEO_ROOT=${TRIPLEO_ROOT:-~/.cache/tripleo} #nodocs
+
+## #. Set the default bare metal power manager. By default devtest uses
+##    nova.virt.baremetal.virtual_power_driver.VirtualPowerManager to
+##    support a fully virtualized TripleO test environment. You may
+##    optionally customize this setting if you are using real baremetal
+##    hardware with the devtest scripts. This setting controls the
+##    power manager used in both the seed VM and undercloud.
+##    ::
+export POWER_MANAGER=${POWER_MANAGER:-'nova.virt.baremetal.virtual_power_driver.VirtualPowerManager'}
+
+## #. Set a list of image elements that should be included in all image builds.
+##    Note that stackuser is only for debugging support - it is not suitable for
+##    a production network. This is also the place to include elements such as
+##    pip-cache or pypi-openstack if you intend to use them.
+##    ::
+export DIB_COMMON_ELEMENTS=${DIB_COMMON_ELEMENTS:-"stackuser"}
+
+## #. Set HW resources for VMs used as 'baremetal' nodes. NODE_CPU is cpu count,
+##    NODE_MEM is memory (MB), NODE_DISK is disk size (GB), NODE_ARCH is
+##    architecture (i386, amd64). NODE_ARCH is used also for the seed VM.
+##    A note on memory sizing: TripleO images in raw form are currently
+##    ~2.7Gb, which means that a tight node will end up with a thrashing page
+##    cache during glance -> local + local -> raw operations. This significantly
+##    impairs performance. Of the four minimum VMs for TripleO simulation, two
+##    are nova baremetal nodes (seed an undercloud) and these need to be 2G or
+##    larger. The hypervisor host in the overcloud also needs to be a decent size
+##    or it cannot host more than one VM.
+## 
+##    32bit VMs::
+## 
+##         export NODE_CPU=1 NODE_MEM=2048 NODE_DISK=20 NODE_ARCH=i386
+export NODE_CPU=${NODE_CPU:-1} NODE_MEM=${NODE_MEM:-2048} NODE_DISK=${NODE_DISK:-20} NODE_ARCH=${NODE_ARCH:-i386} #nodocs
+
+##    For 64bit it is better to create VMs with more memory and storage because of
+##    increased memory footprint::
+## 
+##         export NODE_CPU=1 NODE_MEM=2048 NODE_DISK=20 NODE_ARCH=amd64
+## 
+
+## #. Set distribution used for VMs (fedora, ubuntu).
+##    ::
+## 
+##         export NODE_DIST=ubuntu
+## 
+##    for Fedora set SELinux permissive mode.
+##    ::
+## 
+##         export NODE_DIST="fedora selinux-permissive"
+source $TRIPLEO_ROOT/tripleo-incubator/scripts/set-os-type
+export NODE_DIST=${NODE_DIST:-"$TRIPLEO_OS_DISTRO"}
+
+## #. You need to make the tripleo image elements accessible to diskimage-builder:
+##    ::
+export ELEMENTS_PATH=$TRIPLEO_ROOT/tripleo-image-elements/elements
+
+### --end
