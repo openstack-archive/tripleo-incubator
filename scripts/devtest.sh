88456724f3b198fff8686cb628f25cec9d79d20b
James Slagle <jslagle@redhat.com>
2014-01-21 17:19:53 -0500

    Document stable branches in devtest
    This change adds a warning about stability and the tripleo stable branches to
    devtest. Anyone wanting to make use of the stable branches should heed the
    warning and make sure they are using the correct docs and branches.
    
    Change-Id: I23989fec0f9bcbbc1e47ae6eb2617a8c85afa4ec

--- scripts/devtest.sh
+++ scripts/devtest.sh
@@ -87,6 +87,25 @@ fi
 ## This document is extracted from devtest.sh, our automated bring-up story for
 ## CI/experimentation.
 
+## Stability Warning
+## -----------------
+
+## Note that every effort is made to keep the published set of these instructions
+## updated for use with only the master branches of the TripleO projects. There is
+## **NO** gauranteed stability in master. There is also no gauranteed stable
+## upgrade path from release to release or from one stable branch to a later
+## stable branch.
+
+## If you wish to use the stable branches, you should instead checkout and clone
+## the stable branch of tripleo-incubator you want, and then build the
+## instructions yourself via::
+
+##      git clone https://git.openstack.org/openstack/tripleo-incubator
+##      cd triple-incubator
+##      git checkout <stable-branch>
+##      tox -evenv python setup.py build_sphinx
+##      # View doc/build/html/devtest.html in your browser and proceed from there
+
 ## Next Steps:
 ## -----------
 ## 
