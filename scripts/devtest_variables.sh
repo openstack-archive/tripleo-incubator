ba02f77d18c7bf11638f69d5d0ba33580cfa0841
Gregory Haynes <greg@greghaynes.net>
2014-01-30 11:57:17 -0800

    Make devtest respect COMPUTESCALE value
    The COMPUTESCALE variable allows a variable number of compute nodes to
    be created. Devtest needs to be aware of this and provision the correct
    resources.
    
    Change-Id: Icd5a5ec94dcac7e8fd376da496e4584b436cdd09

--- scripts/devtest_variables.sh
+++ scripts/devtest_variables.sh
@@ -104,6 +104,11 @@ export NODE_CPU=${NODE_CPU:-1} NODE_MEM=${NODE_MEM:-2048} NODE_DISK=${NODE_DISK:
 source $(dirname ${BASH_SOURCE[0]})/set-os-type #nodocs
 export NODE_DIST=${NODE_DIST:-"$TRIPLEO_OS_DISTRO"} #nodocs
 
+## #. Set number of compute nodes for the overcloud
+##    ::
+
+export OVERCLOUD_COMPUTESCALE=${OVERCLOUD_COMPUTESCALE:-1}
+
 ## #. You need to make the tripleo image elements accessible to diskimage-builder:
 ##    ::
 
