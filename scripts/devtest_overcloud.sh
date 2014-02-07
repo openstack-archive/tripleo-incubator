31415f0a5a6eeef9cdab3e1ae5f67e8fb3125d2d
Ralf Haferkamp <rhafer@suse.de>
2014-02-07 15:26:36 +0100

    Use baremetal element to extract kernel and initrd for undercloud
    The "disk-image-get-kernel" tool is deprecated
    
    Change-Id: I2a61330a26a7bea6abc458781c83ec96f4b604e6

--- scripts/devtest_overcloud.sh
+++ scripts/devtest_overcloud.sh
@@ -60,7 +60,7 @@ NODE_ARCH=$(os-apply-config -m $TE_DATAFILE --key arch --type raw)
 if [ ! -e $TRIPLEO_ROOT/overcloud-control.qcow2 -o "$USE_CACHE" == "0" ] ; then #nodocs
     $TRIPLEO_ROOT/diskimage-builder/bin/disk-image-create $NODE_DIST \
         -a $NODE_ARCH -o $TRIPLEO_ROOT/overcloud-control hosts \
-        boot-stack cinder-api cinder-volume os-collect-config \
+        baremetal boot-stack cinder-api cinder-volume os-collect-config \
         neutron-network-node dhcp-all-interfaces swift-proxy swift-storage \
         $DIB_COMMON_ELEMENTS $OVERCLOUD_CONTROL_DIB_EXTRA_ARGS ${SSL_ELEMENT:-} 2>&1 | \
         tee $TRIPLEO_ROOT/dib-overcloud-control.log
@@ -79,7 +79,7 @@ NODE_ARCH=$(os-apply-config -m $TE_DATAFILE --key arch --type raw)
 if [ ! -e $TRIPLEO_ROOT/overcloud-compute.qcow2 -o "$USE_CACHE" == "0" ] ; then #nodocs
     $TRIPLEO_ROOT/diskimage-builder/bin/disk-image-create $NODE_DIST \
         -a $NODE_ARCH -o $TRIPLEO_ROOT/overcloud-compute hosts \
-        nova-compute nova-kvm neutron-openvswitch-agent os-collect-config \
+        baremetal nova-compute nova-kvm neutron-openvswitch-agent os-collect-config \
         dhcp-all-interfaces $DIB_COMMON_ELEMENTS $OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS 2>&1 | \
         tee $TRIPLEO_ROOT/dib-overcloud-compute.log
 fi #nodocs
