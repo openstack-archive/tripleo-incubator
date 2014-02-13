de1dd76a69d2495519427e97de62ff5cd38da6e7
Dirk Mueller <dirk@dmllr.de>
2014-02-13 18:04:08 +0100

    Switch over to oslosphinx
    oslosphinx is now available as a replacement for oslo.sphinx that
    won't conflict with oslo.config in virtual envs
    
    Change-Id: I5462913d8c633239e0f3187de128fd539ff34b34
    Closes-Bug: #1277168

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('./'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'oslo.sphinx',
+    'oslosphinx',
     'ext.extract_docs'
 ]
 
