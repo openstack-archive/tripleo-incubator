3939919bc715232b46a5050d2a906d5d7f5270b2
Ghe Rivero <ghe.rivero@hp.com>
2014-03-25 09:37:49 +0100

    Add elements to user image via TEST_IMAGE_DIB_EXTRA_ARGS
    There are some elements that need to be included in the user image,
    like apt-source and pypi, to be able to build it in a network restricted
    environment.
    
    Change-Id: I90d37a641c5e6ce9b98bf2e8a6cde05ef39f8cc3

--- scripts/devtest_overcloud.sh
+++ scripts/devtest_overcloud.sh
@@ -205,9 +205,10 @@ heat $HEAT_OP -f $TRIPLEO_ROOT/tripleo-heat-templates/overcloud.yaml \
 
 ## #. While we wait for the stack to come up, build an end user disk image.::
 
+TEST_IMAGE_DIB_EXTRA_ARGS=${TEST_IMAGE_DIB_EXTRA_ARGS:-''} #nodocs
 if [ ! -e $TRIPLEO_ROOT/user.qcow2 -o "$USE_CACHE" == "0" ] ; then #nodocs
     if [ "${USE_CIRROS:-0}" = "0" ]; then #nodocs
-        $TRIPLEO_ROOT/diskimage-builder/bin/disk-image-create $NODE_DIST vm \
+        $TRIPLEO_ROOT/diskimage-builder/bin/disk-image-create $NODE_DIST vm $TEST_IMAGE_DIB_EXTRA_ARGS \
             -a $NODE_ARCH -o $TRIPLEO_ROOT/user 2>&1 | tee $TRIPLEO_ROOT/dib-user.log
 ### --end
      else
