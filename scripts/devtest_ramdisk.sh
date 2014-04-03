b6f3fde13aa3a6072337b9385970b46a6e30fb40
Ghe Rivero <ghe.rivero@hp.com>
2014-04-03 13:40:19 +0200

    Encapsulate getting image for a deployment.
    By encapsulating getting of images we can transparently
    download them, or use them from cache, or build them, without
    having to reproduce the logic in every script.
    
    Change-Id: Iae40275bf94c644a552ace98805b75a6f2afcce1
    Co-Authored-By: Robert Collins <rbtcollins@hp.com>

--- scripts/devtest_ramdisk.sh
+++ scripts/devtest_ramdisk.sh
@@ -11,12 +11,15 @@ function show_options () {
     echo "Build a baremetal deployment ramdisk."
     echo
     echo "Options:"
-    echo "      -h             -- this help"
+    echo "      -h                    -- this help"
+    echo "      --download-images URL -- attempt to download images from this URL."
     echo
     exit $1
 }
 
-TEMP=$(getopt -o h -l help -n $SCRIPT_NAME -- "$@")
+DOWNLOAD_OPT=
+
+TEMP=$(getopt -o h -l download-images:,help -n $SCRIPT_NAME -- "$@")
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 
 # Note the quotes around `$TEMP': they are essential!
@@ -24,6 +27,7 @@ eval set -- "$TEMP"
 
 while true ; do
     case "$1" in
+        --download-images) DOWNLOAD_OPT="--download $2"; shift 2;;
         -h | --help) show_options 0;;
         --) shift ; break ;;
         *) echo "Error: unsupported option $1." ; exit 1 ;;
@@ -57,19 +61,26 @@ else
     DEPLOY_NAME=deploy-ramdisk-ironic
 fi
 
-## #. Create a deployment ramdisk + kernel. These are used by the seed cloud and
-##    the undercloud for deployment to bare metal.
+## #. Create or download deployment ramdisk + kernel. These are used by the
+##    seed cloud and the undercloud for deployment to bare metal. To use a
+##    cache pass -c to acquire-image. To download the files pass --download BASE_URL.
 ##    ::
 
-### --end
 NODE_ARCH=$(os-apply-config -m $TE_DATAFILE --key arch)
-if [ ! -e $TRIPLEO_ROOT/$DEPLOY_NAME.kernel -o \
-     ! -e $TRIPLEO_ROOT/$DEPLOY_NAME.initramfs -o \
-     "$USE_CACHE" == "0" ] ; then
-### --include
-    $TRIPLEO_ROOT/diskimage-builder/bin/ramdisk-image-create -a $NODE_ARCH \
-        $NODE_DIST $DEPLOY_IMAGE_ELEMENT -o $TRIPLEO_ROOT/$DEPLOY_NAME \
-        $DIB_COMMON_ELEMENTS 2>&1 | \
-        tee $TRIPLEO_ROOT/dib-deploy.log
+##    acquire-image $TRIPLEO_ROOT/deploy-ramdisk \
+##    $TRIPLEO_ROOT/diskimage-builder/bin/ramdisk-image-create -- \
+##    -a $NODE_ARCH $NODE_DIST $DEPLOY_IMAGE_ELEMENT \
+##    $DIB_COMMON_ELEMENTS
+
 ### --end
+
+if [ "$USE_CACHE" = "1" ]; then
+    CACHE_OPT=-c
+else
+    CACHE_OPT=
 fi
+acquire-image $CACHE_OPT $DOWNLOAD_OPT $TRIPLEO_ROOT/deploy-ramdisk \
+    $TRIPLEO_ROOT/diskimage-builder/bin/ramdisk-image-create -- \
+    -a $NODE_ARCH $NODE_DIST $DEPLOY_IMAGE_ELEMENT \
+    $DIB_COMMON_ELEMENTS 2>&1 | \
+    tee $TRIPLEO_ROOT/dib-deploy.log
