246b5ae538b13dc32b2806b5b9018edcabcf81f0
Michael Kerrin <michael.kerrin@hp.com>
2014-04-04 14:50:23 +0100

    The default in heat-templates is to preserve ephemeral disks
    There is no point in duplicating where we set this policy.
    
    Change-Id: I995cf744ff8838673d2845993dc7441560e3fb36

--- scripts/devtest_overcloud.sh
+++ scripts/devtest_overcloud.sh
@@ -71,14 +71,6 @@ DIB_COMMON_ELEMENTS=${DIB_COMMON_ELEMENTS:-'stackuser'}
 OVERCLOUD_CONTROL_DIB_EXTRA_ARGS=${OVERCLOUD_CONTROL_DIB_EXTRA_ARGS:-'rabbitmq-server'}
 OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS=${OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS:-''}
 TE_DATAFILE=${TE_DATAFILE:?"TE_DATAFILE must be defined before calling this script!"}
-# This will stop being a parameter once rebuild ``--preserve-ephemeral`` is fully
-# merged. For now, it requires manual effort to use, so it should be opt-in.
-# Since it's not an end-user thing yet either, we don't document it in the
-# example prose below either.
-# The patch sets needed are:
-# nova: I6bf01e52589c5894eb043f2b57e915d52e81ebc3
-# python-novaclient: Ib1511653904d4f95ab03fb471669175127004582
-OVERCLOUD_IMAGE_UPDATE_POLICY=${OVERCLOUD_IMAGE_UPDATE_POLICY:-'REBUILD'}
 
 # A client-side timeout in minutes for creating or updating the overcloud
 # Heat stack.
@@ -293,7 +285,6 @@ NeutronControlPlaneID=$(neutron net-show ctlplane | grep ' id ' | awk '{print $4
 ENV_JSON=$(jq '.parameters = {
     "ControlVirtualInterface": "'${OVERCLOUD_VIRTUAL_INTERFACE}'"
   } + .parameters + {
-    "ImageUpdatePolicy": "'${OVERCLOUD_IMAGE_UPDATE_POLICY}'",
     "NeutronPublicInterfaceDefaultRoute": "'${NeutronPublicInterfaceDefaultRoute}'",
     "NeutronPublicInterfaceIP": "'${NeutronPublicInterfaceIP}'",
     "NeutronPublicInterfaceRawDevice": "'${NeutronPublicInterfaceRawDevice}'",
