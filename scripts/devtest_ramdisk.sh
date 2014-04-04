175beac9f884a9db221975ca3e8c7afad7aab07b
Robert Collins <rbtcollins@hp.com>
2014-04-04 15:06:03 +1300

    Make devtest_* accept options.
    The current positional-only interface is super hard to work with.
    While this patch doesn't add any new options it makes it possible
    to do so without resorting to backdoor variables.
    
    Change-Id: I7e75c462525dd1ffbcd310face5861c6358e9d71

--- scripts/devtest_ramdisk.sh
+++ scripts/devtest_ramdisk.sh
@@ -2,6 +2,33 @@
 
 set -eux
 set -o pipefail
+SCRIPT_NAME=$(basename $0)
+SCRIPT_HOME=$(dirname $0)
+
+function show_options () {
+    echo "Usage: $SCRIPT_NAME [options]"
+    echo
+    echo "Build a baremetal deployment ramdisk."
+    echo
+    echo "Options:"
+    echo "      -h             -- this help"
+    echo
+    exit $1
+}
+
+TEMP=$(getopt -o h -l help -n $SCRIPT_NAME -- "$@")
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$TEMP"
+
+while true ; do
+    case "$1" in
+        -h | --help) show_options 0;;
+        --) shift ; break ;;
+        *) echo "Error: unsupported option $1." ; exit 1 ;;
+    esac
+done
 
 USE_CACHE=${USE_CACHE:-0}
 DIB_COMMON_ELEMENTS=${DIB_COMMON_ELEMENTS:-'stackuser'}
