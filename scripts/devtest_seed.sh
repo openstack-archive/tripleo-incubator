e4cddf865670aabd7a4ac943f482b8498fc6cd00
Robert Collins <rbtcollins@hp.com>
2014-04-07 05:53:21 +1200

    Remove boot-seed-vm need to know ssh key.
    This has always been a bit of a layering oddness - other hypervisors
    don't check ssh works. We may be able to remove the ssh glue
    altogether, but this is more conservative - I just remove the need
    to actually log in. An additional plus is that this is cheaper -
    we're not doing a full ssh handshake.
    
    I also noticed some plain wrong prose about what boot-seed-vm does
    vis-a-vis SSH keys, so fixed that up.
    
    Change-Id: Ic357dc882308de95f4f69e5596cfd4896ba6085a

--- scripts/devtest_seed.sh
+++ scripts/devtest_seed.sh
@@ -141,10 +141,9 @@ fi
 ## #. If you're just building images, you're done with this script. Move on
 ##    to :doc:`devtest_undercloud`
 
-##    ``boot-seed-vm`` will start a VM and copy your SSH pub key into the VM so that
-##    you can log into it with ``ssh stack@192.0.2.1``.
-
-##    The IP address of the VM is printed out at the end of boot-seed-vm, or
+##    ``boot-seed-vm`` will start a VM containing your SSH key for the root user.
+## 
+##    The IP address of the VM's eth0 is printed out at the end of boot-seed-vm, or
 ##    you can query the testenv json which is updated by boot-seed-vm::
 
 SEED_IP=$(OS_CONFIG_FILES=$TE_DATAFILE os-apply-config --key seed-ip --type netaddress)
