e2c5971e34b7b2c6affea778a37b78a9fa3fb4f0
Coleman Corrigan <coleman.corrigan@hp.com>
2014-04-14 15:13:20 +0100

    Use config baremetal seed ip addr not 192.0.2.1
    Instead of using a hardcoded 192.0.2.1 address, extract the baremetal
     ip address from the config with os-apply-config, and use this when
     removing the ssh key, so that new seed instances have a new ssh key.
    The intent here is to start the way towards configurable bare metal
    seed addresses.
    
    Change-Id: I2c1f19c8dbcbbcdf40f9a8f78e904f9133fd32c2

--- scripts/devtest.sh
+++ scripts/devtest.sh
@@ -247,7 +247,8 @@ DEVTEST_RD_END=$(date +%s) #nodocs
 ##    with the seed cloud.::
 
 ##         devtest_seed.sh
-##         export no_proxy=${no_proxy:-},192.0.2.1
+##         export no_proxy=${no_proxy:-},$(os-apply-config --type netaddress -m \
+##             $TE_DATAFILE --key baremetal-network.seed.ip --key-default '192.0.2.1')
 ##         source $TRIPLEO_ROOT/tripleo-incubator/seedrc
 
 ### --end
@@ -259,7 +260,8 @@ else
 fi
 devtest_seed.sh $BUILD_ONLY $ALLNODES
 DEVTEST_SD_END=$(date +%s)
-export no_proxy=${no_proxy:-},192.0.2.1
+export no_proxy=${no_proxy:-},$(os-apply-config --type netaddress -m $TE_DATAFILE \
+                                --key baremetal-network.seed.ip --key-default '192.0.2.1')
 if [ -z "$BUILD_ONLY" ]; then
     source $TRIPLEO_ROOT/tripleo-incubator/seedrc
 fi
