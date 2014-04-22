811d3512f9b098da995cd3fcffbe30cb568b562f
Phil Neal <phil.neal@hp.com>
2014-04-22 08:00:50 -0700

    Add Ceilometer to control node by default
    Change-Id: I275402fa914c11cccb7b131a5eac2d663c4cf9ff

--- scripts/devtest_overcloud.sh
+++ scripts/devtest_overcloud.sh
@@ -110,9 +110,10 @@ fi
 if [ ! -e $TRIPLEO_ROOT/overcloud-control.qcow2 -o "$USE_CACHE" == "0" ] ; then #nodocs
     $TRIPLEO_ROOT/diskimage-builder/bin/disk-image-create $NODE_DIST \
         -a $NODE_ARCH -o $TRIPLEO_ROOT/overcloud-control hosts \
-        baremetal boot-stack cinder-api cinder-volume cinder-tgt \
-        os-collect-config horizon neutron-network-node dhcp-all-interfaces \
-        swift-proxy swift-storage \
+        baremetal boot-stack cinder-api cinder-volume cinder-tgt ceilometer-collector\
+        ceilometer-api ceilometer-agent-central ceilometer-agent-notification \
+        ceilometer-agent-compute os-collect-config horizon neutron-network-node \
+        dhcp-all-interfaces swift-proxy swift-storage \
         $DIB_COMMON_ELEMENTS $OVERCLOUD_CONTROL_DIB_EXTRA_ARGS ${SSL_ELEMENT:-} 2>&1 | \
         tee $TRIPLEO_ROOT/dib-overcloud-control.log
 fi #nodocs
