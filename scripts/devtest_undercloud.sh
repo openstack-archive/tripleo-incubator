80b851b40d7996c51c54f38960399da5f08c5d83
Nicholas Randon <nicholas.randon@hp.com>
2014-05-06 11:59:19 +0100

    Choose whether to deploy/update using heat.
    Let the undercloud script to chose whether to deploy/update the
    stack using heat. based on:
    I26f91980971fd4598b043bdb764359ac32a3c42a
    
    Change-Id: Iaa6a11c080dd77f45b7a2e85cbb06d2769947be0

--- scripts/devtest_undercloud.sh
+++ scripts/devtest_undercloud.sh
@@ -164,6 +164,23 @@ fi
 
 STACKNAME_UNDERCLOUD=${STACKNAME_UNDERCLOUD:-'undercloud'}
 
+## #. Choose whether to deploy or update. Use stack-update to update::
+
+##         HEAT_OP=stack-create
+
+### --end
+
+if heat stack-show $STACKNAME_UNDERCLOUD > /dev/null; then
+    HEAT_OP=stack-update
+    if (heat stack-show $STACKNAME_UNDERCLOUD | grep -q FAILED); then
+        echo "Cannot update a failed stack" >&2
+        exit 1
+    fi
+else
+    HEAT_OP=stack-create
+fi
+
+
 ## #. We need an environment file to store the parameters we're gonig to give
 ##    heat.::
 
@@ -202,7 +219,7 @@ jq . > "${HEAT_ENV}" <<< $ENV_JSON
 
 make -C $TRIPLEO_ROOT/tripleo-heat-templates $HEAT_UNDERCLOUD_TEMPLATE
 
-heat stack-create -e $HEAT_ENV \
+heat $HEAT_OP -e $HEAT_ENV \
     -f $TRIPLEO_ROOT/tripleo-heat-templates/$HEAT_UNDERCLOUD_TEMPLATE \
     $STACKNAME_UNDERCLOUD
 
