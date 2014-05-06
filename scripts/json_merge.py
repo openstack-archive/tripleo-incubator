8f5e2e71c5e8d842ba89f1e12184e939bbcd76d1
Michael Tupitsyn <mikhail.tupitsyn@hp.com>
2014-05-06 09:36:23 -0700

    Handy utility to deep merge JSON strings
    Deep-merge JSON strings provided via a command line. This can be
    utilized to compose a JSON string for $OVERCLOUD_EXTRA_CONFIG
    environment variable used for "passthrough" feature.
    
    Change-Id: If292e490d7446f4f9ab5744526beaebfe91e20d5

---
+++ scripts/json_merge.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+''' 
+Deep-merge JSON-formatted strings provided via a command line. Arrays met on the
+same level and same path are concatenated. In case if only one string provided, 
+this utility can be used as JSON formatter (with -p).
+'''
+
+import argparse
+import json
+import types
+
+parser = argparse.ArgumentParser(description = 'Handy utility to merge JSON strings')
+parser.add_argument('-p', '--pretty', action = 'store_true', help = 'pretty-format JSON output')
+parser.add_argument('json_string', nargs='+', help = 'JSON string to merge')
+args = parser.parse_args()
+
+def dict_merge(d1, d2):
+    '''Performs a merge of 2 dictionaries, potentially with recursion'''
+    for k2 in d2.keys():
+        if d1.has_key(k2) and type(d1[k2]) is types.DictType and type(d2[k2]) is types.DictType:
+            dict_merge(d1[k2], d2[k2])
+        elif d1.has_key(k2) and type(d1[k2]) is types.ListType and type(d2[k2]) is types.ListType:
+            d1[k2] = d1[k2] + d2[k2]
+        else:
+            d1[k2] = d2[k2]
+
+if __name__ == '__main__':
+
+    out = json.loads(args.json_string[0])
+    for j in args.json_string[1:]:
+        dict_merge(out, json.loads(j))
+
+    if args.pretty:
+        print json.dumps(out, sort_keys=True, indent=4)
+    else:
+        print json.dumps(out)
