9e018b6a381b64b7a63f624004aa2cdde9dc5f9b
Loganathan Parthipan <parthipan@hp.com>
2014-05-07 12:28:43 +0000

    scripts to persist a running devtest to disk
    These scripts allow one to save all the devtest VMs in a tight loop
    to the disk and resume them later. The new scripts are:
    
    devtest_save.sh
    devtest_resume.sh
    devtest_setup_route.sh
    
    This is useful if one wants to take down the host for maintenance.
    
    Change-Id: I9d1e3f1abc35f614e30944487eaec9a5dcc2ea49

---
+++ scripts/devtest_save.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+### --include
+## devtest_save
+## ============
+
+## #. Save your devtest instances to disk
+
+## This script allows one to save all the devtest instances to disk and ready to
+## resumed at a later time. It is required that the instance be mangedsave by
+## libvirt/qemu. One of the known issue is that an instance connected to a sata
+## virtual bus cannot be persisted to disk. Therefore, please make sure
+## DISK_BUS_TYPE is set to 'scsi' in the environment.
+### --end
+
+## #. Get the list of devtest VMs from libvirt
+
+##    ::
+
+baremetals=( $(sudo virsh list --name --state-running | grep "^baremetal" ) )
+readarray -t reverse_baremetals < <(for a in "${baremetals[@]}"; do echo "$a"; done | sort -r)
+seeds=( $(sudo virsh list --name --state-running| grep "^seed" ) )
+readarray -t reverse_seeds < <(for a in "${seeds[@]}"; do echo "$a"; done | sort -r)
+
+
+## #. First pause the VMs in reverse order. This stage is needs to be done in a
+##    tight loop to avoid any clock skews or the state inconsistencies.
+
+##    ::
+for v in ${reverse_baremetals[@]} ${reverse_seeds[@]}; do
+    echo "Pausing " $v
+    sudo virsh suspend $v
+done
+
+## #. Sleep for 2 seconds. No logical justification, simply being paranoid.
+##    ::
+sleep 2
+
+## #. Now save the VMs to disk. VM order doesn't matter now
+##    though we still do it in a reverse order.
+##    ::
+for v in ${reverse_baremetals[@]} ${reverse_seeds[@]}; do
+    echo "Saving " $v
+    sudo virsh managedsave $v
+done
+
+echo "Saving devtest instances completed."
+echo ""
+
+## #. You can reboot your workstation now. To resume devtest
+##    run devtest_resume.sh
+
+##    ::
+
+
+## The End!
+##
+### --end
