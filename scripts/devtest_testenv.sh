d8ce6e5959ca352aaab90a3ddb5182fc9263149a
Jon-Paul Sullivan <jonpaul.sullivan@hp.com>
2014-05-14 15:49:35 +0100

    Revert to correctly determining nodes needed
    Change Icff5e728148f2744f3fc7c6e99d744be9fda180b set the number of
    nodes created for a test environment to a fixed value for reasons
    that were not well explained in the commit message.
    
    This effectively broke the ability of the OVERCLOUD_*SCALE variables
    to operate correctly and also created a large number of VMs on
    developer workstations unnecessarily.
    
    After discussion in IRC it was stated that a typical developer workflow
    was made easier if more vms than needed were created by the testenv
    script, so this change creates a minimum of 15 vms, but also honours
    the pre-existing NODE_CNT variable and will create enough vms for the
    settings of OVERCLOUD_*SCALE, or error.
    
    Closes-bug: 1319473
    Change-Id: I93c09e1905ca4eb6e9e580ecd0e9d4b8fcf1339e

--- scripts/devtest_testenv.sh
+++ scripts/devtest_testenv.sh
@@ -218,11 +218,32 @@ else
 ##    more nodes.
 ##    ::
 
-# Node definitions are cheap but redeploying testenv's is not.
-# Set NODE_CNT high enough for typical CI and Dev deployments for the
-# forseeable future
-NODE_CNT=${NODE_CNT:-15}
+# Currently devtest.sh uses a single seed, a single undercloud node, and then
+# variables for the overcloud scale
+#     Seed  + Undercloud + Overcloud Controllers + Overcloud Compute Nodes
+# There is also a desire to have a minimum of 15 nodes created, so cater for
+# all cases.
+### --end
+TMP_CNT=$(( 1 + 1 + $OVERCLOUD_CONTROLSCALE + $OVERCLOUD_COMPUTESCALE ))
+(( $TMP_CNT < 15 )) || NODE_CNT=${NODE_CNT:-$TMP_CNT}
 
+### --include
+NODE_CNT=${NODE_CNT:-15}
+### --end
+
+# At this point we are either honouring the environment NODE_CNT or have set it to
+# the required value if 1 + 1 + $OVERCLOUD_CONTROLSCALE + $OVERCLOUD_COMPUTESCALE > 15
+# otherwise the it will be set to the minimum of 15 or honour the environment setting.
+# If NODE_CNT now won't satisfy our OVERCLOUD_*SCALE settings error, becase we will likely
+# fail later anyway.
+if (( $NODE_CNT < $TMP_CNT )) ; then
+    echo "ERROR: Not enough VMs will be created for the requested scale"
+    echo "$TMP_CNT needed, but $NODE_CNT being created"
+    echo "$TMP_CNT is created from 1 + 1 + OVERCLOUD_CONTROLSCALE + OVERCLOUD_COMPUTESCALE"
+    exit 1
+fi
+
+### --include
 create-nodes $NODE_CPU $NODE_MEM $NODE_DISK $NODE_ARCH $NODE_CNT $SSH_USER $HOSTIP $JSONFILE $BRIDGE
 ### --end
 fi
