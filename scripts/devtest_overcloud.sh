b77a2f2ef4ce7ecafb468f2158321048d15d2e7c
Phil Neal <phil.neal@hp.com>
2014-05-21 08:32:49 -0600

    Fix missing Ceilometer password in overcloud deployment
    Updates to overcloud deploy script inadvertently left out Ceilometer password. Adding back in.
    
    Change-Id: I6e8954f5508c91d903370a2a8eb08be4935265d2
    Closes-Bug: #1321784

--- scripts/devtest_overcloud.sh
+++ scripts/devtest_overcloud.sh
@@ -244,6 +244,7 @@ fi
 ENV_JSON=$(jq '.parameters += {
     "AdminPassword": "'"${OVERCLOUD_ADMIN_PASSWORD}"'",
     "AdminToken": "'"${OVERCLOUD_ADMIN_TOKEN}"'",
+    "CeilometerPassword": "'"${OVERCLOUD_CEILOMETER_PASSWORD}"'",
     "CinderPassword": "'"${OVERCLOUD_CINDER_PASSWORD}"'",
     "CloudName": "'"${OVERCLOUD_NAME}"'",
     "GlancePassword": "'"${OVERCLOUD_GLANCE_PASSWORD}"'",
