898de9fe1f85f84f7c00545e6a36b13a1cd781e4
Haiwei Xu <xu-haiwei@mxw.nes.nec.co.jp>
2014-06-23 19:19:43 +0900

    Make location of saved credentials files customerizable
    Save undercloud/overcloud-passwords in $TRIPLEO_ROOT by default.
    User can also specify the path by changing the parameters in
    devtest_variables.
    
    Change-Id: Ifca80a1d9bc9aff61aee8c70e67f59451d1bb55b

--- scripts/devtest_variables.sh
+++ scripts/devtest_variables.sh
@@ -163,4 +163,10 @@ export ELEMENTS_PATH=${ELEMENTS_PATH:-"$TRIPLEO_ROOT/tripleo-image-elements/elem
 
 export TE_DATAFILE=${TE_DATAFILE:-"$TRIPLEO_ROOT/testenv.json"}
 
+## #. Set the path to save undercloud and overcloud passwords.
+##    ::
+
+export UNDERCLOUD_PASSWORDS_FILE=${UNDERCLOUD_PASSWORDS_FILE:-"$TRIPLEO_ROOT/tripleo-undercloud-passwords"}
+export OVERCLOUD_PASSWORDS_FILE=${OVERCLOUD_PASSWORDS_FILE:-"$TRIPLEO_ROOT/tripleo-overcloud-passwords"}
+
 ### --end
