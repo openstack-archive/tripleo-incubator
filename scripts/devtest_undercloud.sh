e381d07b369e49dd1d05e43d48ba79f0b50f354b
Alexis Lee <alexisl@hp.com>
2014-06-24 15:12:05 +0100

    Wait longer for BM node registration
    At present the code waits for `nova hypervisor-stats` to show the
    expected number of nodes are registered. However, at this stage the
    nodes can be only partially registered. This means that when the
    scheduler is asked for a host, it doesn't know EG that there is ram
    available and the ram filter fails.
    
    By also waiting for memory to be registered this patch should improve
    the timing characteristics and avoid this failure.
    
    Change-Id: I2c08bb3dee44f002442fd5c764d5dffed47ee9ba

--- scripts/devtest_undercloud.sh
+++ scripts/devtest_undercloud.sh
@@ -151,7 +151,7 @@ POWER_USER=$(os-apply-config -m $TE_DATAFILE --key ssh-user --type raw)
 
 ## #. Wait for the BM cloud to register BM nodes with the scheduler::
 
-wait_for 60 1 [ "\$(nova hypervisor-stats | awk '\$2==\"count\" { print \$4}')" != "0" ]
+wait_for 60 1 wait_for_hypervisor_stats
 
 
 ## #. We need an environment file to store the parameters we're gonig to give
