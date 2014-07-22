56b1cc26676ea3b00736b9488ac7c26b07746090
stephane <stephane@alum.mit.edu>
2014-07-22 11:28:10 -0700

    Write tripleorc in devtest_variables.sh
    Currently, tripleorc doesn't get written until devtest_end.sh is run.
    This means that if devtest fails prior to the end, the tripleorc
    won't get written. This change moves the tripleorc write to
    devtest_variables.sh so it will get written before a failure may
    occur.
    
    Change-Id: I7390a5585b078fda0b15ce28d032cf6ba52609f7

--- scripts/devtest_end.sh
+++ scripts/devtest_end.sh
@@ -7,24 +7,10 @@ set -o pipefail
 ## devtest_end
 ## ============
 
-## #. Save your devtest environment::
-
-##      write-tripleorc --overwrite $TRIPLEO_ROOT/tripleorc
-
-### --end
-if [ -e tripleorc ]; then
-  echo "Resetting existing $PWD/tripleorc with new values"
-  tripleorc_path=$PWD/tripleorc
-else
-  tripleorc_path=$TRIPLEO_ROOT/tripleorc
-fi
-write-tripleorc --overwrite $tripleorc_path
-
 echo "devtest.sh completed."
 echo source $tripleorc_path to restore all values
 echo ""
 
-### --include
 ## #. If you need to recover the environment, you can source tripleorc.
 ##    ::
 
