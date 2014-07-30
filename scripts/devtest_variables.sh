431ac00e1d40f65a921b1cd0f6d205412927d1fb
James Slagle <jslagle@redhat.com>
2014-07-30 14:16:07 -0400

    Make USE_MARIADB overrideable
    The $USE_MARIADB environment variable needs to be overrideable otherwise
    settings in tripleorc/devtestrc are not going to have any effect.
    
    Change-Id: I3e4bb4005068b2b5143b2f5d7eecd6ef1b4406ad

--- scripts/devtest_variables.sh
+++ scripts/devtest_variables.sh
@@ -209,9 +209,9 @@ export TE_DATAFILE=${TE_DATAFILE:-"$TRIPLEO_ROOT/testenv.json"}
 
 
 if [[ $NODE_DIST =~ .*(fedora|rhel|centos).* ]] ; then
-    export USE_MARIADB=1
+    export USE_MARIADB=${USE_MARIADB:-1}
 else
-    export USE_MARIADB=0
+    export USE_MARIADB=${USE_MARIADB:-0}
 fi
 
 ### --end
