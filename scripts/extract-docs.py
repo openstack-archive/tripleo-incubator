316ec524bb0e267e6b13be147316b639a09a1124
James Polley <jp@jamezpolley.com>
2014-08-05 07:02:18 +0200

    Replace extract-docs.awk with extract-docs.py
    extract-docs.py has several nice features over extract-docs.awk. The
    obvious advantage is that it's easier for python developers to read than
    the awk script (not that the awk script was particularly hard)
    
    More importantly, this script has the property that it preserves line
    numbers - so when you get a failure on line 206 of devtest.rst, that
    means the error is on line 206 of devtest.sh, rather than being on some
    random other line number.
    
    I'd like to see this script integrated into Sphinx as a dedicated
    pre-processor, but in the meantime this should help us make errors
    building docs easier to read.
    
    Change-Id: Ibb6f943e3f6b30f1bdbc0b8d7b73f7b32f75fc6a

---
+++ scripts/extract-docs.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+
+"""Pre-processor for munging files before inestion by sphinx.
+
+Read a file with embedded comments and:
+ - discard undesired portions
+ - strip leading '## ' from lines
+ - indent other non-empty lines by 8 spaces
+
+This allows a script to have copious documentation but also be presented as a
+markdown / ReST file.
+
+
+"""
+
+from __future__ import print_function
+
+import argparse
+import re
+
+
+def main():
+    """Process FILENAME for ingestion by sphinx.
+
+    If begin-enabled is true, processing will start from the first line.
+    Otherwise processing will start from the first --include"""
+
+    parser = argparse.ArgumentParser(
+        description="Process a file for ingestion by Sphinx")
+    parser.add_argument('filename', help="Name of file to ingest")
+    parser.add_argument('--begin_enabled', type=bool, default=False)
+
+    args = parser.parse_args()
+
+    # filename, begin_enabled=False)
+    _enabled = args.begin_enabled
+
+    with open(args.filename) as infile:
+        for line in infile:
+            output = None
+            if re.search("^### --include", line):
+                _enabled = True
+                output = "\n"
+            elif re.search("^### --end", line):
+                _enabled = False
+                output = "\n"
+            elif re.search("#nodocs$", line):
+                output = "\n"
+            elif re.match("^$", line):
+                output = line
+
+            if not output:
+                if not _enabled:
+                    output = "\n"
+                elif re.match("## ", line):
+                    output = line[3:]
+                else:
+                    output = "%s%s" % ("        ", line)
+
+            print(output, end="")
+
+if __name__ == '__main__':
+    main()
