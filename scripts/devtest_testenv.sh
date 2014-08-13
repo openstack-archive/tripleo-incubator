b8fb78cc0751792efb8e84fa4bebb571d46e5b3f
Jan Provaznik <jprovazn@redhat.com>
2014-08-13 21:50:15 +0200

    Increase default RAM for 32bit to 2.5G
    After merging patches which include pacemaker and corosync services
    in undercloud and overcloud, VMs may run out of memory quite often.
    Increasing default memory to 2.5G should make situation more stable.
    
    Change-Id: Ib68a5d542620fadd2b93b87bf8b59115f4d7423c

--- scripts/devtest_testenv.sh
+++ scripts/devtest_testenv.sh
@@ -104,9 +104,9 @@ fi
 ##    32bit VMs
 ##    ::
 
-##         NODE_CPU=1 NODE_MEM=2048 NODE_DISK=40 NODE_ARCH=i386
+##         NODE_CPU=1 NODE_MEM=2572 NODE_DISK=40 NODE_ARCH=i386
 
-NODE_CPU=${NODE_CPU:-1} NODE_MEM=${NODE_MEM:-2048} NODE_DISK=${NODE_DISK:-40} NODE_ARCH=${NODE_ARCH:-i386} NODE_CNT=${NODE_CNT:-15} #nodocs
+NODE_CPU=${NODE_CPU:-1} NODE_MEM=${NODE_MEM:-2572} NODE_DISK=${NODE_DISK:-40} NODE_ARCH=${NODE_ARCH:-i386} NODE_CNT=${NODE_CNT:-15} #nodocs
 
 ##    For 64bit it is better to create VMs with more memory and storage because of
 ##    increased memory footprint (3GB minimum - we suggest 4GB)::
