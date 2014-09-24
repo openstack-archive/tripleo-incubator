b5ff5996b1069f3de7da37c55af54e330ee4aca3
Tomas Sedovic <tsedovic@redhat.com>
2014-09-24 17:19:45 +0200

    Pass resource registry file to Heat if it exists
    If the tripleo-heat-templates repository contains the
    "overcloud-resource-registry.yaml" environment file, we pass it to Heat
    using the `-e` switch (in addition to the "parameter" environment file
    we're passing in already).
    
    This will let us test (and eventually merge) the change
    I687a00c7dc164ba044f9f2dfca96a02401427855.
    
    Passing multiple environment files was added in python-heatclient
    version 0.2.11.
    
    Change-Id: Ib82eecf0998ae5c0a811a248b638c5633c8bb262

--- scripts/devtest_overcloud.sh
+++ scripts/devtest_overcloud.sh
@@ -350,6 +350,22 @@ ENV_JSON=$(jq '.parameters = {
     "SnmpdReadonlyUserPassword": "'${UNDERCLOUD_CEILOMETER_SNMPD_PASSWORD}'",
   }' <<< $ENV_JSON)
 
+RESOURCE_REGISTRY=
+RESOURCE_REGISTRY_PATH="$TRIPLEO_ROOT/tripleo-heat-templates/overcloud-resource-registry.yaml"
+
+if [ -e "$RESOURCE_REGISTRY_PATH" ]; then
+    RESOURCE_REGISTRY="-e $RESOURCE_REGISTRY_PATH"
+    ENV_JSON=$(jq '.parameters = .parameters + {
+        "ControllerCount": '${OVERCLOUD_CONTROLSCALE}',
+        "ComputeCount": '${OVERCLOUD_COMPUTESCALE}'
+      }' <<< $ENV_JSON)
+    if [ -e "$TRIPLEO_ROOT/tripleo-heat-templates/cinder-storage.yaml" ]; then
+        ENV_JSON=$(jq '.parameters = .parameters + {
+            "BlockStorageCount": '${OVERCLOUD_BLOCKSTORAGESCALE}'
+          }' <<< $ENV_JSON)
+    fi
+fi
+
 ### --include
 
 ## #. Save the finished environment file.::
@@ -378,6 +394,7 @@ make -C $TRIPLEO_ROOT/tripleo-heat-templates overcloud.yaml \
 # create stack with a 6 hour timeout, and allow wait_for_stack_ready
 # to impose a realistic timeout.
 heat $HEAT_OP -e "$HEAT_ENV" \
+    $RESOURCE_REGISTRY \
     -t 360 \
     -f $TRIPLEO_ROOT/tripleo-heat-templates/overcloud.yaml \
     -P "ExtraConfig=${OVERCLOUD_EXTRA_CONFIG}" \
