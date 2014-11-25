d811a0060782224cc534250e52fcb04c7797e4b8
Giulio Fidente <gfidente@redhat.com>
2014-11-25 07:00:42 -0500

    Add support for building Ceph nodes and configure Cinder to use it
    Introduces support for CEPHSTORAGESCALE, meant to define the number
    of Ceph OSDs to deploy and CEPHSTORAGE_FLAVOR to configure the nova
    flavor to use for the Ceph OSD nodes.
    
    Also adds the logic which enables usage of Ceph elements and
    configuration of the Cinder backend if CEPHSTORAGESCALE is > 0
    
    Change-Id: I6afadbcf7b17b11ec1a78e84cb1a4732d2646e2f
    Depends-On: I9d5f9da4e411acfb55658a104e061a97fc82be1a
    Depends-On: I954861536c41b2a7e6cbd86a0f0b55004eed4c70

--- scripts/devtest_overcloud.sh
+++ scripts/devtest_overcloud.sh
@@ -13,6 +13,7 @@ COMPUTE_FLAVOR="baremetal"
 CONTROL_FLAVOR="baremetal"
 BLOCKSTORAGE_FLAVOR="baremetal"
 SWIFTSTORAGE_FLAVOR="baremetal"
+CEPHSTORAGE_FLAVOR="baremetal"
 
 function show_options () {
     echo "Usage: $SCRIPT_NAME [options]"
@@ -38,11 +39,14 @@ function show_options () {
     echo "       --swift-storage-flavor -- Nova flavor to use for swift "
     echo "                                 storage nodes."
     echo "                                 Defaults to 'baremetal'."
+    echo "       --ceph-storage-flavor  -- Nova flavor to use for ceph "
+    echo "                                 storage nodes."
+    echo "                                 Defaults to 'baremetal'."
     echo
     exit $1
 }
 
-TEMP=$(getopt -o c,h -l build-only,no-mergepy,debug-logging,heat-env:,compute-flavor:,control-flavor:,block-storage-flavor:,swift-storage-flavor:,help -n $SCRIPT_NAME -- "$@")
+TEMP=$(getopt -o c,h -l build-only,no-mergepy,debug-logging,heat-env:,compute-flavor:,control-flavor:,block-storage-flavor:,swift-storage-flavor:,ceph-storage-flavor:,help -n $SCRIPT_NAME -- "$@")
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 
 # Note the quotes around `$TEMP': they are essential!
@@ -63,6 +67,7 @@ while true ; do
         --control-flavor) CONTROL_FLAVOR="$2"; shift 2;;
         --block-storage-flavor) BLOCKSTORAGE_FLAVOR="$2"; shift 2;;
         --swift-storage-flavor) SWIFTSTORAGE_FLAVOR="$2"; shift 2;;
+        --ceph-storage-flavor) CEPHSTORAGE_FLAVOR="$2"; shift 2;;
         -h | --help) show_options 0;;
         --) shift ; break ;;
         *) echo "Error: unsupported option $1." ; exit 1 ;;
@@ -99,18 +104,26 @@ SSL_ELEMENT=${SSLBASE:+openstack-ssl}
 USE_CACHE=${USE_CACHE:-0}
 DIB_COMMON_ELEMENTS=${DIB_COMMON_ELEMENTS:-'stackuser'}
 OVERCLOUD_CONTROL_DIB_ELEMENTS=${OVERCLOUD_CONTROL_DIB_ELEMENTS:-'ntp hosts baremetal boot-stack cinder-api ceilometer-collector ceilometer-api ceilometer-agent-central ceilometer-agent-notification ceilometer-alarm-notifier ceilometer-alarm-evaluator os-collect-config horizon neutron-network-node dhcp-all-interfaces swift-proxy swift-storage keepalived haproxy sysctl'}
-OVERCLOUD_CONTROL_DIB_EXTRA_ARGS=${OVERCLOUD_CONTROL_DIB_EXTRA_ARGS:-'rabbitmq-server cinder-tgt'}
+OVERCLOUD_CONTROL_DIB_EXTRA_ARGS=${OVERCLOUD_CONTROL_DIB_EXTRA_ARGS:-'rabbitmq-server'}
 OVERCLOUD_COMPUTE_DIB_ELEMENTS=${OVERCLOUD_COMPUTE_DIB_ELEMENTS:-'ntp hosts baremetal nova-compute nova-kvm neutron-openvswitch-agent os-collect-config dhcp-all-interfaces sysctl'}
 OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS=${OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS:-''}
 
 OVERCLOUD_BLOCKSTORAGE_DIB_ELEMENTS=${OVERCLOUD_BLOCKSTORAGE_DIB_ELEMENTS:-'ntp hosts baremetal os-collect-config dhcp-all-interfaces sysctl'}
 OVERCLOUD_BLOCKSTORAGE_DIB_EXTRA_ARGS=${OVERCLOUD_BLOCKSTORAGE_DIB_EXTRA_ARGS:-'cinder-tgt'}
+OVERCLOUD_CEPHSTORAGE_DIB_EXTRA_ARGS=${OVERCLOUD_CEPHSTORAGE_DIB_EXTRA_ARGS:-''}
 TE_DATAFILE=${TE_DATAFILE:?"TE_DATAFILE must be defined before calling this script!"}
 
 if [ "${USE_MARIADB:-}" = 1 ] ; then
     OVERCLOUD_CONTROL_DIB_EXTRA_ARGS="$OVERCLOUD_CONTROL_DIB_EXTRA_ARGS mariadb-rpm"
     OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS="$OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS mariadb-dev-rpm"
     OVERCLOUD_BLOCKSTORAGE_DIB_EXTRA_ARGS="$OVERCLOUD_BLOCKSTORAGE_DIB_EXTRA_ARGS mariadb-dev-rpm"
+fi
+
+if [ "${USE_CEPH:-}" = 1 ] ; then
+    OVERCLOUD_CONTROL_DIB_EXTRA_ARGS="$OVERCLOUD_CONTROL_DIB_EXTRA_ARGS ceph-mon cinder-volume-ceph"
+    OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS="$OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS nova-compute-volume-ceph"
+else
+    OVERCLOUD_CONTROL_DIB_EXTRA_ARGS="$OVERCLOUD_CONTROL_DIB_EXTRA_ARGS cinder-tgt"
 fi
 
 # A client-side timeout in minutes for creating or updating the overcloud
@@ -131,8 +144,8 @@ OVERCLOUD_FIXED_RANGE_NAMESERVER=${OVERCLOUD_FIXED_RANGE_NAMESERVER:-"8.8.8.8"}
 ##    This is the image the undercloud
 ##    will deploy to become the KVM (or QEMU, Xen, etc.) cloud control plane.
 
-##    ``$OVERCLOUD_*_DIB_EXTRA_ARGS`` (CONTROL, COMPUTE, BLOCKSTORAGE) are
-##    meant to be used to pass additional build-time specific arguments to
+##    ``$OVERCLOUD_*_DIB_EXTRA_ARGS`` (CONTROL, COMPUTE, BLOCKSTORAGE and CEPHSTORAGE)
+##    are meant to be used to pass additional build-time specific arguments to
 ##    ``disk-image-create``.
 
 ##    ``$SSL_ELEMENT`` is used when building a cloud with SSL endpoints - it should be
@@ -148,6 +161,7 @@ if [ "$USE_UNDERCLOUD_UI" -ne 0 ] ; then
     OVERCLOUD_CONTROL_DIB_EXTRA_ARGS="$OVERCLOUD_CONTROL_DIB_EXTRA_ARGS snmpd"
     OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS="$OVERCLOUD_COMPUTE_DIB_EXTRA_ARGS snmpd"
     OVERCLOUD_BLOCKSTORAGE_DIB_EXTRA_ARGS="$OVERCLOUD_BLOCKSTORAGE_DIB_EXTRA_ARGS snmpd"
+    OVERCLOUD_CEPHSTORAGE_DIB_EXTRA_ARGS="$OVERCLOUD_CEPHSTORAGE_DIB_EXTRA_ARGS snmpd"
 fi
 
 if [ ! -e $TRIPLEO_ROOT/overcloud-control.qcow2 -o "$USE_CACHE" == "0" ] ; then #nodocs
@@ -201,7 +215,30 @@ if [ $OVERCLOUD_DISTRIBUTED_ROUTERS == "True" ]; then
    OVERCLOUD_COMPUTE_DIB_ELEMENTS=$OVERCLOUD_COMPUTE_DIB_ELEMENTS$ComputeNeutronRole
 fi
 
-## #. Create your overcloud compute image. This is the image the undercloud
+## #. Create your ceph storage image if some ceph storage nodes are to be used. This
+##    is the image the undercloud deploys for the dedicated instances running ceph-osd.
+##    ::
+
+if [ $OVERCLOUD_CEPHSTORAGESCALE -gt 0 ]; then
+    if [ ! -e $TRIPLEO_ROOT/overcloud-ceph-storage.qcow2 -o "$USE_CACHE" == "0" ]; then #nodocs
+        $TRIPLEO_ROOT/diskimage-builder/bin/disk-image-create $NODE_DIST \
+            -a $NODE_ARCH -o $TRIPLEO_ROOT/overcloud-ceph-storage ntp hosts \
+            baremetal os-collect-config \
+            dhcp-all-interfaces ceph-osd $DIB_COMMON_ELEMENTS \
+            $OVERCLOUD_CEPHSTORAGE_DIB_EXTRA_ARGS 2>&1 | \
+            tee $TRIPLEO_ROOT/dib-overcloud-ceph-storage.log
+    fi #nodocs
+
+## #. Load the image into Glance, unless you are just building the images.
+
+##    ::
+
+    if [ -z "$BUILD_ONLY" ]; then #nodocs
+    OVERCLOUD_CEPHSTORAGE_ID=$(load-image -d $TRIPLEO_ROOT/overcloud-ceph-storage.qcow2)
+    fi #nodocs
+fi
+
+## #. Create your compute image. This is the image the undercloud
 ##    deploys to host KVM (or QEMU, Xen, etc.) instances.
 ##    ::
 
@@ -299,7 +336,7 @@ if (neutron net-list | grep -q public); then
     VLAN_GW=$(neutron subnet-show $PUBLIC_SUBNET_ID | awk '/gateway_ip/ { print $4}')
     BM_VLAN_CIDR=$(neutron subnet-show $PUBLIC_SUBNET_ID | awk '/cidr/ { print $4}')
     NeutronPublicInterfaceDefaultRoute="${VLAN_GW}"
-    export CONTROLEXTRA=overcloud-vlan-port.yaml
+    export CONTROLEXTRA="${CONTROLEXTRA:-} overcloud-vlan-port.yaml"
 else
     VLAN_ID=
     NeutronPublicInterfaceTag=
@@ -331,7 +368,7 @@ fi
 
 ## #. Wait for the BM cloud to register BM nodes with the scheduler::
 
-expected_nodes=$(( $OVERCLOUD_COMPUTESCALE + $OVERCLOUD_CONTROLSCALE + $OVERCLOUD_BLOCKSTORAGESCALE ))
+expected_nodes=$(( $OVERCLOUD_COMPUTESCALE + $OVERCLOUD_CONTROLSCALE + $OVERCLOUD_BLOCKSTORAGESCALE + $OVERCLOUD_CEPHSTORAGESCALE ))
 wait_for -w $((60 * $expected_nodes)) --delay 10 -- wait_for_hypervisor_stats $expected_nodes
 
 ## #. Set password for Overcloud SNMPd, same password needs to be set in Undercloud Ceilometer
@@ -443,6 +480,29 @@ if [ $OVERCLOUD_DISTRIBUTED_ROUTERS == "True" ]; then
     }' <<< $ENV_JSON)
 fi
 
+# NOTE(gfidente): we do not check against CEPHSTORAGESCALE because
+# ceph-osd could have been deployed on controllers (CI) and instead
+# we want to enable the settings relevant to Ceph together with HA
+function create_cephx_key() {
+    # Taken from https://github.com/ceph/ceph-deploy/blob/15b348e502128df435f8c2c7073e8b936086e325/ceph_deploy/new.py#L21
+    local KEY=$(python -c 'import base64,os,struct,time;key = os.urandom(16);header = struct.pack("<hiih", 1, int(time.time()), 0, len(key));print(base64.b64encode(header + key))')
+    echo $KEY
+}
+
+if [ $OVERCLOUD_CEPHSTORAGESCALE -gt 1 ]; then
+    FSID=$(uuidgen)
+    # CephX keys https://github.com/ceph/ceph-deploy/blob/15b348e502128df435f8c2c7073e8b936086e325/ceph_deploy/new.py#L21
+    MON_KEY=$(create_cephx_key)
+    ADMIN_KEY=$(create_cephx_key)
+    ENV_JSON=$(jq '.parameters = {} + .parameters + {
+        "OvercloudCephStorageFlavor": "'"${CEPHSTORAGE_FLAVOR}"'",
+        "CephStorageImage": "'"${OVERCLOUD_CEPHSTORAGE_ID}"'",
+        "CephClusterFSID": "'"${FSID}"'",
+        "CephMonKey": "'"${MON_KEY}"'",
+        "CephAdminKey": "'"${ADMIN_KEY}"'",
+      }' <<< $ENV_JSON)
+fi
+
 ### --end
 # Options we haven't documented as such
 ENV_JSON=$(jq '.parameters = {
@@ -466,6 +526,11 @@ if [ "$USE_MERGEPY" -eq 0 ]; then
     if [ -e "$TRIPLEO_ROOT/tripleo-heat-templates/cinder-storage.yaml" ]; then
         ENV_JSON=$(jq '.parameters = .parameters + {
             "BlockStorageCount": '${OVERCLOUD_BLOCKSTORAGESCALE}'
+          }' <<< $ENV_JSON)
+    fi
+    if [ -e "$TRIPLEO_ROOT/tripleo-heat-templates/ceph-storage.yaml" ]; then
+        ENV_JSON=$(jq '.parameters = .parameters + {
+            "CephStorageCount": '${OVERCLOUD_CEPHSTORAGESCALE}'
           }' <<< $ENV_JSON)
     fi
 fi
@@ -494,7 +559,8 @@ if [ "$USE_MERGEPY" -eq 1 ]; then
     make -C $TRIPLEO_ROOT/tripleo-heat-templates overcloud.yaml \
             COMPUTESCALE=$OVERCLOUD_COMPUTESCALE,${OVERCLOUD_COMPUTE_BLACKLIST:-} \
             CONTROLSCALE=$OVERCLOUD_CONTROLSCALE,${OVERCLOUD_CONTROL_BLACKLIST:-} \
-            BLOCKSTORAGESCALE=$OVERCLOUD_BLOCKSTORAGESCALE
+            BLOCKSTORAGESCALE=$OVERCLOUD_BLOCKSTORAGESCALE \
+            CEPHSTORAGESCALE=$OVERCLOUD_CEPHSTORAGESCALE
     OVERCLOUD_TEMPLATE=$TRIPLEO_ROOT/tripleo-heat-templates/overcloud.yaml
 else
     OVERCLOUD_TEMPLATE=$TRIPLEO_ROOT/tripleo-heat-templates/overcloud-without-mergepy.yaml
--- scripts/devtest_variables.sh
+++ scripts/devtest_variables.sh
@@ -124,17 +124,13 @@ fi
 ##    control node. It is not required for overcloud computes. The backend is
 ##    set through the ``*EXTRA_ARGS``.
 ##    rabbitmq-server is enabled by default. Another option is qpidd.
-##    For overclouds we also use ``*EXTRA_ARGS`` to choose a cinder backend, set
-##    to cinder-tgt by default.
-##    ::
 
 export SEED_DIB_EXTRA_ARGS=${SEED_DIB_EXTRA_ARGS:-"rabbitmq-server"}
 export UNDERCLOUD_DIB_EXTRA_ARGS=${UNDERCLOUD_DIB_EXTRA_ARGS:-"rabbitmq-server"}
-export OVERCLOUD_CONTROL_DIB_EXTRA_ARGS=${OVERCLOUD_CONTROL_DIB_EXTRA_ARGS:-'rabbitmq-server cinder-tgt'}
+export OVERCLOUD_CONTROL_DIB_EXTRA_ARGS=${OVERCLOUD_CONTROL_DIB_EXTRA_ARGS:-'rabbitmq-server'}
 
 ## #. The block storage nodes are deployed with the cinder-tgt backend by
-##    default too. Alteratives are cinder-lio and cinder-volume-nfs. Make sure
-##    to check the README files of these elements to configure them as needed.
+##    default. Another option is cinder-lio.
 ##    ::
 
 export OVERCLOUD_BLOCKSTORAGE_DIB_EXTRA_ARGS=${OVERCLOUD_BLOCKSTORAGE_DIB_EXTRA_ARGS:-'cinder-tgt'}
@@ -188,13 +184,14 @@ export ROOT_DISK=${ROOT_DISK:-10}
 
 export LIBVIRT_DISK_BUS_TYPE=${LIBVIRT_DISK_BUS_TYPE:-"sata"}
 
-## #. Set number of compute, control and block storage nodes for the overcloud.
+## #. Set number of compute, control and other type of nodes for the overcloud.
 ##    Only a value of 1 for OVERCLOUD_CONTROLSCALE is currently supported.
 ##    ::
 
 export OVERCLOUD_COMPUTESCALE=${OVERCLOUD_COMPUTESCALE:-2}
 export OVERCLOUD_CONTROLSCALE=${OVERCLOUD_CONTROLSCALE:-1}
 export OVERCLOUD_BLOCKSTORAGESCALE=${OVERCLOUD_BLOCKSTORAGESCALE:-0}
+export OVERCLOUD_CEPHSTORAGESCALE=${OVERCLOUD_CEPHSTORAGESCALE:-0}
 
 ## #. These optional variables can be set to remove dead nodes. See the merge.py
 ##    help for details of use. These example lines would remove Compute1 and
@@ -227,6 +224,22 @@ if [[ $NODE_DIST =~ .*(fedora|rhel|centos).* ]] ; then
     export USE_MARIADB=${USE_MARIADB:-1}
 else
     export USE_MARIADB=0
+fi
+
+
+## #. Set ``USE_CEPH=1`` if you want Cinder to be configured with Ceph as its
+##    backend. Enabled by default when ``OVERCLOUD_CEPHSTORAGESCALE`` is > 0
+##    ::
+
+
+if [ $OVERCLOUD_CEPHSTORAGESCALE -gt 0 ] ; then
+    export USE_CEPH=${USE_CEPH:-1}
+else
+    export USE_CEPH=0
+fi
+
+if [ $USE_CEPH -eq 1 ]; then
+    export CONTROLEXTRA="${CONTROLEXTRA:-} ceph-source.yaml"
 fi
 
 
