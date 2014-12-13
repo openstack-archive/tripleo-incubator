ca22523d6d732fe6395d15c8eb3b71d5691feab8
James Polley <jp@jamezpolley.com>
2014-12-13 10:36:24 +0100

    Support --help as well as -h
    The majority of scripts in this directory (17 vs 14) support both -h and
    --help. Switch this script to be part of the majority.
    
    Help text is modified to clarify the fact that --bm-networks is
    optional, but if not supplied the script is a no-op
    
    Change-Id: I1c57f522ea3b26969538674d0cbc6c7be4bba365

--- scripts/devtest_update_network.sh
+++ scripts/devtest_update_network.sh
@@ -5,15 +5,20 @@ set -o pipefail
 SCRIPT_NAME=$(basename $0)
 
 function show_options () {
-    echo "Usage: $SCRIPT_NAME [options] {JSON-filename}"
+    echo "Usage: $SCRIPT_NAME --bm-networks NETFILE {JSON-filename}"
     echo
-    echo "Updates the BM network description for a TripleO devtest environment."
+    echo "Reads the baremetal-network description in NETFILE and writes it into JSON-filename"
+    echo
+    echo "For instance, to read the file named bm-networks.json and update testenv.json:"
+    echo "      ${SCRIPT_NAME} --bm-networks bm-networks.json testenv.json "
     echo
     echo "Options:"
     echo "    -h                     -- This help."
     echo "    --bm-networks NETFILE  -- You are supplying your own network layout."
     echo "                              The schema for baremetal-network can be found in"
     echo "                              the devtest_setup documentation."
+    echo "                              For backwards compatibility, this argument is optional;"
+    echo "                              but if it's not provided this script does nothing."
     echo
     echo "JSON-filename -- the path to write the environment description to."
     echo
@@ -22,7 +27,7 @@ function show_options () {
 
 NETS_PATH=
 
-TEMP=$(getopt -o h -l bm-networks: -n $SCRIPT_NAME -- "$@")
+TEMP=$(getopt -o h -l help,bm-networks: -n $SCRIPT_NAME -- "$@")
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 
 # Note the quotes around `$TEMP': they are essential!
@@ -31,7 +36,7 @@ eval set -- "$TEMP"
 while true ; do
     case "$1" in
         --bm-networks) NETS_PATH="$2"; shift 2;;
-        -h) show_options 0;;
+        -h|--help) show_options 0;;
         --) shift ; break ;;
         *) echo "Error: unsupported option $1." ; exit 1 ;;
     esac
