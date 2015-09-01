195edd45462874a619e363ccfe1cbc1c6584b5e1
Dan Prince <dprince@redhat.com>
2015-08-31 22:20:24 -0400

    Updates to support glanceclient v1_0
    This updates the devtest scripts so they support using
    --visibility instead of --is-public.
    
    Add --container-format to places where glance is called
    as its now required.
    
    Use "nova image-show" in place of glance, as glance now
    only take an image id and no longer accepts the image name.
    
    Change-Id: Id44afde796c5aa660628f7f8890f74e2024288af
    Closes-bug: #1490792

--- scripts/devtest_overcloud.sh
+++ scripts/devtest_overcloud.sh
@@ -637,7 +637,7 @@ EOF
 ## #. Register the end user image with glance.
 ##    ::
 
-    glance image-create --name user --is-public True --disk-format qcow2 \
+    glance image-create --name user --visibility public --disk-format qcow2 \
         --container-format bare --file $TRIPLEO_ROOT/$USER_IMG_NAME
 
 fi #nodocs
@@ -667,7 +667,7 @@ if [ "stack-create" = "$HEAT_OP" ] ; then #nodocs
 ## #. So that you can deploy a VM.
 ##    ::
 
-    IMAGE_ID=$(glance image-show user | awk '/ id / {print $4}')
+    IMAGE_ID=$(nova image-show user | awk '/ id / {print $4}')
     nova boot --key-name default --flavor m1.tiny --block-device source=image,id=$IMAGE_ID,dest=volume,size=3,shutdown=preserve,bootindex=0 demo
 
 ## #. Add an external IP for it.
